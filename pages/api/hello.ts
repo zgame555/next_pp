281f26b1b40996c688c0d865d3f1f82416f3d5dd
pm <premwit@outlook.co.th>
2023-07-08 04:51:08 +0700

    add

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,38 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-import type { NextApiRequest, NextApiResponse } from 'next'
+import type { NextApiRequest, NextApiResponse } from "next";
+import puppeteer from "puppeteer";
 
 type Data = {
-  name: string
-}
+  name: string;
+};
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<Data>
+  res: NextApiResponse
 ) {
-  res.status(200).json({ name: 'John Doe' })
+  const { url } = req.query;
+
+  if (!url) {
+    return res.status(400).json({ error: "URL parameter is required" });
+  }
+
+  let browser;
+
+  try {
+    browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
+    await page.goto(String(url));
+
+    // load end
+
+    const image = await page.screenshot({ type: "png" });
+
+    res.status(200).send(image);
+  } catch (error: any) {
+    res.status(500).json({ error: error?.message });
+  } finally {
+    if (browser) {
+      await browser.close();
+    }
+  }
 }
